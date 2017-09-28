9fea906646e3f29440e8d793ca7ee9a2fad87e80
Steve Baker <stevebaker@steves-mbp-2.gateway>
2017-09-28 14:04:02 +1000

    Initial Commit

---
+++ vision/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  vision
+//
+//  Created by Steve Baker on 28/9/17.
+//  Copyright © 2017 SGB Imagery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
